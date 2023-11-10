30c904f6f0f2b63528f8976866e9e179114b6d3c
qinlu1024 <qinlu1024@users.noreply.github.com>
2023-11-10 01:07:31 +0000

    ✅ Ready to clone and code.

--- fast_case/app.py
+++ fast_case/app.py
@@ -23,18 +23,18 @@ def read(*paths, **kwargs):
 
 
 description = """
-project_name API helps you do awesome stuff. 🚀
+fast_case API helps you do awesome stuff. 🚀
 """
 
 app = FastAPI(
-    title="project_name",
+    title="fast_case",
     description=description,
     version=read("VERSION"),
-    terms_of_service="http://project_name.com/terms/",
+    terms_of_service="http://fast_case.com/terms/",
     contact={
-        "name": "author_name",
-        "url": "http://project_name.com/contact/",
-        "email": "author_name@project_name.com",
+        "name": "qinlu1024",
+        "url": "http://fast_case.com/contact/",
+        "email": "qinlu1024@fast_case.com",
     },
     license_info={
         "name": "The Unlicense",
